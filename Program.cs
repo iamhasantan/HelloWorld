7f373d1abb44f731f45d57ce7abf606b9443cf33
Hasan Tan <hasan@hasantan.com>
2022-09-26 17:19:30 +1000

    Upload
    Upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var helloMessage = "Hello, World!";
+string firstName;
+string lastName = "Tan";
+
+firstName = "Hasan";
+
+Console.Write(helloMessage);
+Console.WriteLine(" The current time is " + DateTime.Now);
+Console.Write("My name is " + firstName + " " + lastName + " bye bye cruel world!!");
