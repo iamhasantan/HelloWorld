ce004571c134b9075b7fb637dc7bc8fad662797d
Hasan Tan <hasan@hasantan.com>
2022-09-26 17:24:23 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿var helloMessage = "Hello, World!";
+﻿//setting some variables
+var helloMessage = "Hello, World!";
 string firstName;
 string lastName = "Tan";
 
+// my name is
 firstName = "Hasan";
+
+// output the message, show the current time and utilise prior set strings to show my name isn't eminem
 
 Console.Write(helloMessage);
 Console.WriteLine(" The current time is " + DateTime.Now);
-Console.Write("My name is " + firstName + " " + lastName + " bye bye cruel world!!");
+Console.Write("Hi my name is " + firstName + " " + lastName + " bye bye cruel world!!");
